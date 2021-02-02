cae65468e2c7d613b347a079ac233c8a5feecb6f
Maximo Machado <maximomachado246@gmail.com>
2021-02-01 22:14:38 -0500

    Set filetype of scratch buffer to filetype of current buffer

--- lua/vimperfect.lua
+++ lua/vimperfect.lua
@@ -25,13 +25,15 @@ local function randomize_buffer(lines)
 end
 
 local function vimperfect()
+
+    local current_buffer_filetype = vim.fn.nvim_buf_get_option(0, 'filetype')
     local current_buffer_lines = vim.fn.nvim_buf_get_lines(0, 0, -1, true)
     local new_buffer = vim.fn.nvim_create_buf(true, true)
 
     -- Set new buffer that is a copy of the current buffer to display in current window
     vim.fn.nvim_buf_set_lines(new_buffer, 0, -1, true, randomize_buffer(current_buffer_lines))
+    vim.fn.nvim_buf_set_option(new_buffer, 'filetype', current_buffer_filetype)
     vim.fn.nvim_win_set_buf(0, new_buffer)
-    return 0
 end
 
 
